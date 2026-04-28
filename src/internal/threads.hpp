c5a13cd7d786d0c8c5f005055a89f4ed9770c0a9
LadyDarknes <sptovaknjo@gmail.com>
2026-04-28 05:00:29 +0300

    Basic moduler verification for threads

--- src/internal/threads.hpp
+++ src/internal/threads.hpp
@@ -1,1 +1,9 @@
 #pragma once
+#include <Windows.h>
+#include <cstdint>
+
+namespace threads
+{
+    //threads::validate_thread_start_addresses(); 
+    void validate_thread_start_addresses(); //EVERYTHING WILL HAVE UNDERLINE :D! :D! :D!
+}
